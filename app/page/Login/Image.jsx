e1df162f0d45c2e785cc7d1993336df693e74372
RaziMaulana <razifaturmaulana@gmail.com>
2025-05-30 13:36:08 +0800

    done login

---
+++ app/page/Login/Image.jsx
@@ -0,0 +1,15 @@
+import { Image, StyleSheet, View } from 'react-native'
+
+export default function ImageSection(){
+    return(
+        <View style={styles.container}>
+            <Image source={require('@/assets/images/CodePlay-Login-Image.png')}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: "10%"
+    }
+})
